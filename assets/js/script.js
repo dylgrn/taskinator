75760ea05c905e66867cd60d0b417a6a67406923
dylan green <dylgrn.github.io>
2021-04-10 20:04:46 -0500

    fixed browser behavoir

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttoneEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttoneEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
